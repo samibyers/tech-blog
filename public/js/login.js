9d081392f6066ac9112c97ca574a13f92d0ceeea
Sami Byers <samtaylor.byers@gmail.com>
2021-12-03 12:11:31 -0600

    fixed login issue

--- public/js/login.js
+++ public/js/login.js
@@ -1,22 +1,23 @@
 const loginForm = document.querySelector(".login-form");
 
-// loginForm.addEventListener("submit",(e) => {
-//     e.preventDefault();
-//     const userObj={
-//         username:document.querySelector("#username-login").value,
-//         password:document.querySelector("#password-login").value,
-//     }
-//     fetch("/api/users/login",{
-//         method:"POST",
-//         body:JSON.stringify(userObj),
-//         headers:{
-//             "Content-Type":"application/json"
-//         }
-//     }).then(res=>{
-//         if(res.ok){
-//            location.href = "/profile"
-//         } else {
-//             alert("Error")
-//         }
-//     }) 
-// });
+loginForm.addEventListener("submit",(e) => {
+    e.preventDefault();
+    console.log("hello")
+    const userObj={
+        username:document.querySelector("#username-login").value,
+        password:document.querySelector("#password-login").value,
+    }
+    fetch("/api/users/login",{
+        method:"POST",
+        body:JSON.stringify(userObj),
+        headers:{
+            "Content-Type":"application/json"
+        }
+    }).then(res=>{
+        if(res.ok){
+           location.href = "/"
+        } else {
+            alert("Error")
+        }
+    }) 
+});
